9d06b5377a10b239997ff4ac45cfe2666f4136c5
sujang958 <enter7377@naver.com>
2025-04-08 08:23:48 +0900

    conf: add a font

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,2 @@
-<script>
-	import Button from '$lib/components/ui/button/button.svelte';
-</script>
-
-<Button>Hello</Button>
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
